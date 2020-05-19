fbd02a2979732ad196eb3db87628d9d8d4ba7841
Jeff Yourman <jeffreyyourman@gmail.com>
2020-05-18 23:54:40 -0400

    created route for ppower"

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ const {
   syncInstruments,
 } = require("./models/jobs/allInstruments.jobs");
 // const { dividendJob } = require("./models/jobs/dividends.jobs");
-// const { paymentsJob } = require("./models/jobs/payments.jobs");
+const { 
+  // paymentsJob,
+  syncPurchasingPower
+} = require("./models/jobs/payments.jobs");
 // const cron = require("node-cron");
 const cors = require("cors");
 
 app.use(cors());
+const SYNCDATA = false;
 
 app.get("/api/instru", async (req, res) => {
   var getDocument = () => {
@@ -23,7 +27,6 @@ app.get("/api/instru", async (req, res) => {
     });
   };
   const allInstruments = await getDocument();
-  // console.log('allInstruments',allInstruments[0].data.length);
   res.json({ myInstruments: allInstruments[0].data });
 });
 
@@ -41,12 +44,10 @@ app.get("/api/payment-history", async (req, res) => {
     var year = new Date(payment.expected_landing_date).getFullYear();
     return year >= 2020 && payment;
   });
-  console.log("paymentHistory", paymentHistory);
   res.json({ response: paymentHistory });
 });
 
 app.get("/api/dividend-history", async (req, res) => {
-  // console.log('hihihi')
   var getDividendHistory = () => {
     return new Promise((resolve, reject) => {
       db.dividends.find(function (err, data) {
@@ -55,7 +56,6 @@ app.get("/api/dividend-history", async (req, res) => {
     });
   };
   const dividendHistory = await getDividendHistory();
-  // console.log('dividendHistory',dividendHistory[0].data.length);
   res.json({ response: dividendHistory[0].data });
 });
 
@@ -75,12 +75,25 @@ app.get("/api/stock-earnings-date/:ticker", async (req, res) => {
   // https://api.nasdaq.com/api/company/DIS/earnings-surprise
   // forcast
   const ticker = req.params.ticker;
-  console.log("ticker", ticker);
   const earningsResponse = await axios.get(
     `https://api.nasdaq.com/api/analyst/${ticker}/earnings-date`
   );
   earningsResponse.data.data.ticker = ticker;
   res.json({ response: earningsResponse.data });
+});
+
+app.get("/api/p-power", async (req, res) => {
+  var getDocument = () => {
+    return new Promise((resolve, reject) => {
+      db.purchasePower.find(function (err, data) {
+        err ? reject(err) : resolve(data);
+      });
+    });
+  };
+  const purchasePower = await getDocument();
+  console.log('purchasePower',purchasePower);
+  console.log('purchasePower',purchasePower[0].data[0]);
+  res.json({ purchasePower: purchasePower[0].data });
 });
 
 // cron.schedule(
@@ -98,7 +111,10 @@ app.get("/api/stock-earnings-date/:ticker", async (req, res) => {
 // cron.schedule(
 //   "0 19 * * * *",
 //   async () => {
-// syncInstruments();
+  if(SYNCDATA) {
+    syncInstruments();
+    syncPurchasingPower();
+  }
 //   },
 //   {
 //     scheduled: true,
